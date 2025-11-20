2d7ea469a1bbfc18fafa68f1b4d714cbebfe485b
Nguyen Tan Vinh <nguyentanvinh0403@gmail.com>
2025-11-20 12:24:07 +0700

    fix call video with camera

--- VideoCall/Application/Interfaces/IUserService.cs
+++ VideoCall/Application/Interfaces/IUserService.cs
@@ -5,7 +5,7 @@ namespace VideoCall.Application.Interfaces
     public interface IUserService
     {
         Task<User?> LoginAsync(string name, string password);
-        Task SetOnlineAsync(string connectionId, User user);
+        Task SetOnlineAsync(string userId, string connectionId);
         Task<User?> SetOfflineAsync(string connectionId); // Sửa đổi
         Task<List<User>> GetOnlineFriendsAsync(string currentUserId);
         User? GetByConnectionId(string connectionId);
